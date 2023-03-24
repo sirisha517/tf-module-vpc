c39de2b31e0932f0877b73f920fe74c46627f5c3
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-24 19:59:40 +0530

    vpc

--- main.tf
+++ main.tf
@@ -89,10 +89,10 @@ resource "aws_route_table" "private-route-table" {
 vpc_id = aws_vpc.main.id
 
 for_each = var.private_subnets
-#route {
-#cidr_block     = "0.0.0.0/0"
-#nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
-#}
+route {
+cidr_block     = "0.0.0.0/0"
+nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
+}
 tags = merge(
 var.tags,
 { Name = "${var.env}-${each.value["name"]}" }
