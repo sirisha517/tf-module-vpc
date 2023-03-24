2d4a53683e7f785356d0334b4c54beb2d0703529
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-24 19:39:40 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,102 +1,107 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  tags = merge(
-    var.tags,
-    { Name = "${var.env}-vpc"}
-  )
+cidr_block = var.vpc_cidr
+tags = merge(
+var.tags,
+{ Name = "${var.env}-vpc" }
+)
 }
-  ## public subnets
+
+## Public Subnets
 resource "aws_subnet" "public_subnets" {
-  vpc_id     = aws_vpc.main.id
+vpc_id = aws_vpc.main.id
 
-  for_each = var.public_subnets
-  cidr_block = each.value["cidr_block"]
-  availability_zone = each.value["availability_zone"]
-  tags =  merge(
-    var.tags,
-    { Name = "${var.env}-${each.value["name"]}"}
-  )
+for_each          = var.public_subnets
+cidr_block        = each.value["cidr_block"]
+availability_zone = each.value["availability_zone"]
+tags = merge(
+var.tags,
+{ Name = "${var.env}-${each.value["name"]}" }
+)
 }
-##  internet gateway
+
+## Internet Gateway
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags =  merge(
-    var.tags,
-    { Name = "${var.env}-igw"}
-  )
+vpc_id = aws_vpc.main.id
+
+tags = merge(
+var.tags,
+{ Name = "${var.env}-igw" }
+)
 }
 
-# elastic IP
+# elastic Ip
 resource "aws_eip" "nat" {
-  for_each = var.public_subnets
-  vpc      = true
+for_each = var.public_subnets
+vpc      = true
 }
-
-#NAT GATEWAY
+#nat gateway
 resource "aws_nat_gateway" "nat-gateways" {
+for_each      = var.public_subnets
+allocation_id = aws_eip.nat[each.value["name"]].id
+subnet_id     = aws_subnet.public_subnets[each.value["name"]].id
 
-  for_each = var.public_subnets
-  allocation_id = aws_eip.nat[each.value["name"]].id
-  subnet_id     = aws_subnet.public_subnets[each.value["name"]].id
-  tags =  merge(
-    var.tags,
-    { Name = "${var.env}-${each.value["name"]}"}
-  )
+tags = merge(
+var.tags,
+{ Name = "${var.env}-${each.value["name"]}" }
+)
 }
-  ## public route table
+
+
+# Public Route table
 resource "aws_route_table" "public-route-table" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-  for_each = var.public_subnets
-  tags =  merge(
-    var.tags,
-    { Name = "${var.env}-${each.value["name"]}"}
-  )
+vpc_id = aws_vpc.main.id
+
+route {
+cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.igw.id
 }
 
-  ## public route table association
+for_each = var.public_subnets
+tags = merge(
+var.tags,
+{ Name = "${var.env}-${each.value["name"]}" }
+)
+}
+# Public Route table association
 resource "aws_route_table_association" "public-association" {
-
-  for_each = var.public_subnets
-  subnet_id      = lookup(lookup(aws_subnet.public_subnets,each.value["name"],null),"id",null)
-  route_table_id = aws_route_table.public-route-table[each.value["name"]].id
+for_each  = var.public_subnets
+subnet_id = lookup(lookup(aws_subnet.public_subnets, each.value["name"], null), "id", null)
+#subnet_id      = aws_subnet.public_subnets[each.value["name"]].id
+route_table_id = aws_route_table.public-route-table[each.value["name"]].id
 }
 
-
-  ## private subnets
-
+## Private Subnets
 resource "aws_subnet" "private_subnets" {
-  vpc_id     = aws_vpc.main.id
+vpc_id = aws_vpc.main.id
 
-  for_each = var.private_subnets
-  cidr_block = each.value["cidr_block"]
-  availability_zone = each.value["availability_zone"]
-  tags =  merge(
-    var.tags,
-    { Name = "${var.env}-${each.value["name"]}"}
-  )
+for_each          = var.private_subnets
+cidr_block        = each.value["cidr_block"]
+availability_zone = each.value["availability_zone"]
+tags = merge(
+var.tags,
+{ Name = "${var.env}-${each.value["name"]}" }
+)
+
 }
+
 # Private Route table
 resource "aws_route_table" "private-route-table" {
-  vpc_id = aws_vpc.main.id
+vpc_id = aws_vpc.main.id
 
-  for_each = var.private_subnets
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
-  }
-  tags = merge(
-    var.tags,
-    { Name = "${var.env}-${each.value["name"]}" }
-  )
+for_each = var.private_subnets
+route {
+cidr_block     = "0.0.0.0/0"
+nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
 }
+tags = merge(
+var.tags,
+{ Name = "${var.env}-${each.value["name"]}" }
+)
+}
+# Private Route table association
 
-## private route table association
-resource "aws_route_table_association" "private-route-association" {
-  for_each = var.private_subnets
-  subnet_id      = lookup(lookup(aws_subnet.private_subnets,each.value["name"],null),"id",null)
-  route_table_id = aws_route_table.private-route-table[each.value["name"]].id
+resource "aws_route_table_association" "private-association" {
+for_each       = var.private_subnets
+subnet_id      = lookup(lookup(aws_subnet.private_subnets, each.value["name"], null), "id", null)
+route_table_id = aws_route_table.private-route-table[each.value["name"]].id
 }
